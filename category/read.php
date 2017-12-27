1eed587f6a02af47dc4cbe97eb0b98866346bc60
Miguel Angel Calle <root@developweb>
2017-12-27 20:58:19 +0100

    cambio de pruebas con editor atom

--- category/read.php
+++ category/read.php
@@ -2,29 +2,29 @@
 // required header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
- 
+
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/category.php';
- 
-// instantiate database and category object
+
+// instantiate database and category objecto
 $database = new Database();
 $db = $database->getConnection();
- 
+
 // initialize object
 $category = new Category($db);
- 
+
 // query categorys
 $stmt = $category->read();
 $num = $stmt->rowCount();
- 
+
 // check if more than 0 record found
 if($num>0){
- 
+
     // products array
     $categories_arr=array();
     $categories_arr["records"]=array();
- 
+
     // retrieve our table contents
     // fetch() is faster than fetchAll()
     // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
@@ -33,19 +33,19 @@ if($num>0){
         // this will make $row['name'] to
         // just $name only
         extract($row);
- 
+
         $category_item=array(
             "id" => $id,
             "name" => $name,
             "description" => html_entity_decode($description)
         );
- 
+
         array_push($categories_arr["records"], $category_item);
     }
- 
+
     echo json_encode($categories_arr);
 }
- 
+
 else{
     echo json_encode(
         array("message" => "No products found.")
